97e717bb4e4d627cbbc69b3e50a066968f6721f1
nobel7761 <habiburnobel@gmail.com>
2024-06-14 14:30:06 +0600

    neon text is added

--- src/components/Home.component.tsx
+++ src/components/Home.component.tsx
@@ -1,10 +1,18 @@
+import { neon_font } from "@/fonts";
 import TopCover from "./shared/TopCover.component";
 
 const HomeComponent = () => {
   return (
     <>
       <TopCover height="h-[75rem]" imageUrl="/banner-background.jpg">
-        <h1 className="text-white ">hello bangladesh</h1>
+        <>
+          <h1 className="text-white ">hello bangladesh</h1>
+          <h1
+            className={`font-bold text-6xl uppercase text-neon ${neon_font.className}`}
+          >
+            FIND YOUR HUMANPOWER
+          </h1>
+        </>
       </TopCover>
       <h2>this is check</h2>
     </>
--- src/fonts/index.tsx
+++ src/fonts/index.tsx
@@ -1,5 +1,6 @@
-import { Inter, Noto_Sans, Space_Grotesk } from "next/font/google";
+import { Inter, Noto_Sans, Space_Grotesk, Tilt_Neon } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const noto_sans = Noto_Sans({ subsets: ["latin"] });
 export const space_grotest = Space_Grotesk({ subsets: ["latin"] });
+export const neon_font = Tilt_Neon({ weight: ["400"], subsets: ["latin"] });
